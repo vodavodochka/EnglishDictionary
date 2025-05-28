fc2401524182f6f352e00a76295d19ebaf7cf398
Max Razin <razinmax02@gmail.com>
2025-05-28 14:46:49 +0500

    resx файлы заполнены для русского и английского

--- EngishMotherFucker/Resources/Strings/Strings.en.Designer.cs
+++ EngishMotherFucker/Resources/Strings/Strings.en.Designer.cs
@@ -22,14 +22,14 @@ namespace EngishMotherFucker.Resources.Strings {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Strings {
+    public class Strings___Копировать {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Strings() {
+        internal Strings___Копировать() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace EngishMotherFucker.Resources.Strings {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("EngishMotherFucker.Resources.Strings.Strings", typeof(Strings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("EngishMotherFucker.Resources.Strings.Strings - Копировать", typeof(Strings___Копировать).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
